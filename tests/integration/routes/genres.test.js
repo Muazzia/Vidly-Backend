b335ba343ec621e22ef7fffe2e66470ed619e51f
Muaz <you@example.com>
2023-07-28 15:13:57 +0500

    geners

--- tests/integration/routes/genres.test.js
+++ tests/integration/routes/genres.test.js
@@ -7,7 +7,7 @@ let app;
 
 describe('/api/genres', () => {
     beforeEach(() => { ({ app: app, server: server } = require('../../../index')) });
-    afterEach(async () => { server.close(), await Genere.deleteMany({}), await mongoose.connection.close(); })
+    afterEach(async () => { server.close(), await Genere.deleteMany({}) })
 
     describe('Get /', () => {
         it('should return all genres', async () => {
@@ -22,6 +22,21 @@ describe('/api/genres', () => {
     })
 
     describe('Get /:id', () => {
+        it('should return error if object Id in invalid', async () => {
+            const res = await request(server).get('/api/genres/2');
+            expect(res.status).toBe(400);
+        })
 
+        it('should return 404 if object id didnt match', async () => {
+            const res = await request(server).get('/api/genres/64be2a3f2f5d7f3790f09b11')
+            expect(res.status).toBe(404)
+        })
+
+        it('should return valid object if found', async () => {
+            const obj = await Genere.create({ name: 'genre1' });
+            const res = await request(server).get(`/api/genres/${obj._id}`);
+            expect(res.body).toHaveProperty('name', obj.name);
+            expect(mongoose.Types.ObjectId.isValid(res.body._id)).toBeTruthy();
+        })
     })
 })
